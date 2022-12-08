b50d5f958b3a774e62f724392d35c9628e238705
Alejandroq12 <quezada.rodriguez6@gmail.com>
2022-12-07 18:53:16 -0600

    update

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -8,15 +8,17 @@ export default class Pagination extends Component {
 
         this.state = {
             contador: 1
+
         }
     }
-    
     
     render() {
         console.log(this.state.contador)
         return (
             <div>
-                <button type="button" class="btn btn-light" onClick={() => this.setState({contador: this.state.contador -1})}>Preview</button>
+                <button type="button" class="btn btn-light" onClick={() => this.setState({contador: this.state.contador -1 })}>Preview</button>
+
+
                 <button type="button" class="btn btn-primary" onClick={() => this.setState({contador: this.state.contador +1})}>Next</button>
                 <br/>
                 <ListCharacters conteo={this.state.contador}/>
